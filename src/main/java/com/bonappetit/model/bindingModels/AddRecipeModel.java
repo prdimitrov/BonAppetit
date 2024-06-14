ee6aa553eeee391ceb7e87ee6484156c7951b30f
prdimitrov <prdimitrov97@gmail.com>
2024-06-14 12:24:52 +0300

    Added a binding model for the recipies.

---
+++ src/main/java/com/bonappetit/model/bindingModels/AddRecipeModel.java
@@ -0,0 +1,59 @@
+package com.bonappetit.model.bindingModels;
+
+import com.bonappetit.model.entity.Category;
+import com.bonappetit.model.entity.User;
+
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+public class AddRecipeModel {
+
+    @NotNull
+    @Size(min = 2, max = 40, message = "Name length must be between 2 and 40 characters!")
+    private String name;
+
+    @NotNull
+    @Size(min = 2, max = 80, message = "Ingredients length must be between 2 and 80 characters!")
+    private String ingredients;
+
+    @NotNull(message = "You must select a category!")
+    private Category category;
+
+
+    private User addedBy;
+
+    public AddRecipeModel() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getIngredients() {
+        return ingredients;
+    }
+
+    public void setIngredients(String ingredients) {
+        this.ingredients = ingredients;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+
+    public User getAddedBy() {
+        return addedBy;
+    }
+
+    public void setAddedBy(User addedBy) {
+        this.addedBy = addedBy;
+    }
+}
